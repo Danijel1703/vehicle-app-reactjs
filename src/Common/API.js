d682608f180d9b42c0deba299cf62eecbd943936
Danijel1703 <danci.dj@gmail.com>
2022-01-10 10:31:49 +0100

    Update API.js

--- src/Common/API.js
+++ src/Common/API.js
@@ -5,154 +5,160 @@
 const axios = require('axios').default
 const baseUrl = 'https://api.baasic.com/beta/vehicle-app-reactjs'
 
-module.exports.getAllModels = async (numberOfModels, sort) => {
-  try {
-    const response = await axios.get(`${baseUrl}/resources/VehicleModel?rpp=${numberOfModels}&sort=${sort}`)
-    return response.data.item
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-module.exports.getAllMakers = async (numberOfMakers, sort) => {
-  try {
-    const response = await axios.get(`${baseUrl}/resources/VehicleMake?rpp=${numberOfMakers}&sort=${sort}`)
-    return response.data.item
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-module.exports.getCurrentPageModels = async (currentPage, sort) => {
-  try {
-    const response = await axios.get(`${baseUrl}/resources/VehicleModel?page=${currentPage}&rpp=10&sort=${sort}`)
-    return response.data.item
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-module.exports.getNumberOfMakers = async () => {
-  try {
-    const response = await axios.get(`${baseUrl}/resources/VehicleMake/`)
-    return response.data.totalRecords
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-module.exports.getNumberOfModels = async () => {
-  try {
-    const response = await axios.get(`${baseUrl}/resources/VehicleModel/`)
-    return response.data.totalRecords
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-module.exports.getModelById = async (id) => {
-  try {
-    const response = await axios.get(`${baseUrl}/resources/VehicleModel/${id}`)
-    return response.data
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-module.exports.updateSelectedModel = async (id, name, abrv) => {
-  try {
-    // eslint-disable-next-line no-unused-vars
-    const response = await axios.patch(`${baseUrl}/resources/VehicleModel/${id}`, { name, abrv })
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-module.exports.deleteSelectedModel = async (id) => {
-  try {
-    // eslint-disable-next-line no-unused-vars
-    const response = await axios.delete(`${baseUrl}/resources/VehicleModel/${id}`)
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-module.exports.addNewModel = async ({ makerId, model, abrv }) => {
-  try {
-    console.log(abrv)
-    const body = {
-      makeId: makerId,
-      name: model,
-      abrv
+class CreateAPI {
+  async getAllModels (numberOfModels, sort) {
+    try {
+      const response = await axios.get(`${baseUrl}/resources/VehicleModel?rpp=${numberOfModels}&sort=${sort}`)
+      return response.data.item
+    } catch (error) {
+      console.error(error)
     }
-    const response = await axios.post(`${baseUrl}/resources/VehicleModel/`, body)
-  } catch (error) {
-    console.error(error)
   }
-}
 
-module.exports.addNewMaker = async ({ maker, abrv }) => {
-  try {
-    console.log(maker, abrv)
-    const body = {
-      name: maker,
-      abrv
+  async getAllMakers (numberOfMakers, sort) {
+    try {
+      const response = await axios.get(`${baseUrl}/resources/VehicleMake?rpp=${numberOfMakers}&sort=${sort}`)
+      return response.data.item
+    } catch (error) {
+      console.error(error)
     }
-    const response = await axios.post(`${baseUrl}/resources/VehicleMake/`, body)
-  } catch (error) {
-    console.error(error)
+  }
+
+  async getCurrentPageModels (currentPage, sort) {
+    try {
+      const response = await axios.get(`${baseUrl}/resources/VehicleModel?page=${currentPage}&rpp=10&sort=${sort}`)
+      return response.data.item
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async getNumberOfMakers () {
+    try {
+      const response = await axios.get(`${baseUrl}/resources/VehicleMake/`)
+      return response.data.totalRecords
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async getNumberOfModels () {
+    try {
+      const response = await axios.get(`${baseUrl}/resources/VehicleModel/`)
+      return response.data.totalRecords
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async getModelById (id) {
+    try {
+      const response = await axios.get(`${baseUrl}/resources/VehicleModel/${id}`)
+      return response.data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async updateSelectedModel (id, name, abrv) {
+    try {
+      // eslint-disable-next-line no-unused-vars
+      const response = await axios.patch(`${baseUrl}/resources/VehicleModel/${id}`, { name, abrv })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async deleteSelectedModel (id) {
+    try {
+      // eslint-disable-next-line no-unused-vars
+      const response = await axios.delete(`${baseUrl}/resources/VehicleModel/${id}`)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async addNewModel ({ makerId, model, abrv }) {
+    try {
+      console.log(abrv)
+      const body = {
+        makeId: makerId,
+        name: model,
+        abrv
+      }
+      const response = await axios.post(`${baseUrl}/resources/VehicleModel/`, body)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async addNewMaker ({ maker, abrv }) {
+    try {
+      console.log(maker, abrv)
+      const body = {
+        name: maker,
+        abrv
+      }
+      const response = await axios.post(`${baseUrl}/resources/VehicleMake/`, body)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async getSearchInputModel (input) {
+    try {
+      const response = await axios.get(`${baseUrl}/resources/VehicleModel?searchQuery=WHERE name='${input}'`)
+      return response.data.item[0]
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async getMakerById (id) {
+    try {
+      const response = await axios.get(`${baseUrl}/resources/VehicleMake/${id}`)
+      return response.data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async updateSelectedMaker (id, name, abrv) {
+    try {
+      // eslint-disable-next-line no-unused-vars
+      const response = await axios.patch(`${baseUrl}/resources/VehicleMake/${id}`, { name, abrv })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async deleteSelectedMaker (id) {
+    try {
+      // eslint-disable-next-line no-unused-vars
+      const response = await axios.delete(`${baseUrl}/resources/VehicleMake/${id}`)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async deleteMakerModel (id) {
+    try {
+      const deleteMakerModel = await axios.delete(`${baseUrl}/resources/VehicleModel/${id}`)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async getSearchInputMaker (input) {
+    try {
+      const response = await axios.get(`${baseUrl}/resources/VehicleMake?searchQuery=WHERE name='${input}'`)
+      return response.data.item[0]
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
 
-module.exports.getSearchInputModel = async (input) => {
-  try {
-    const response = await axios.get(`${baseUrl}/resources/VehicleModel?searchQuery=WHERE name='${input}'`)
-    return response.data.item[0]
-  } catch (error) {
-    console.error(error)
-  }
-}
+const API = new CreateAPI()
 
-module.exports.getMakerById = async (id) => {
-  try {
-    const response = await axios.get(`${baseUrl}/resources/VehicleMake/${id}`)
-    return response.data
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-module.exports.updateSelectedMaker = async (id, name, abrv) => {
-  try {
-    // eslint-disable-next-line no-unused-vars
-    const response = await axios.patch(`${baseUrl}/resources/VehicleMake/${id}`, { name, abrv })
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-module.exports.deleteSelectedMaker = async (id) => {
-  try {
-    // eslint-disable-next-line no-unused-vars
-    const response = await axios.delete(`${baseUrl}/resources/VehicleMake/${id}`)
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-module.exports.deleteMakerModel = async (id) => {
-  try {
-    const deleteMakerModel = await axios.delete(`${baseUrl}/resources/VehicleModel/${id}`)
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-module.exports.getSearchInputMaker = async (input) => {
-  try {
-    const response = await axios.get(`${baseUrl}/resources/VehicleMake?searchQuery=WHERE name='${input}'`)
-    return response.data.item[0]
-  } catch (error) {
-    console.error(error)
-  }
-}
+export default API
